e72e2e26b01b4ac4478adade2bded0bef7d691d9
Azizbek Toshpulatov <xtoshpul@fi.muni.cz>
2024-04-16 17:41:14 +0200

    nothing special

--- buggy_python_code_solution.py
+++ buggy_python_code_solution.py
@@ -1,5 +1,3 @@
-import sys 
-import os
 import yaml
 import flask
 
@@ -12,8 +10,10 @@ def index():
     url = flask.request.args.get("url")
     return fetch_website(version, url)
 
-        
+
 CONFIG = {"API_KEY": "771df488714111d39138eb60df756e6b"}
+
+
 class Person():
     def __init__(self, name):
         self.name = name
@@ -27,10 +27,10 @@ def fetch_website(urllib_version, url):
     # Import the requested version (2 or 3) of urllib
     exec(f"import urllib{urllib_version} as urllib", globals())
     # Fetch and print the requested URL
- 
-    try: 
+
+    try:
         http = urllib.PoolManager()
-        request = http.request('GET', url)
+        r = http.request('GET', url)
     except:
         print('Exception')
 
@@ -39,7 +39,7 @@ def load_yaml(filename):
     stream = open(filename)
     deserialized_data = yaml.load(stream, Loader=yaml.Loader) #deserializing data
     return deserialized_data
-    
+
 def authenticate(password):
     # Assert that the password is correct
     assert password == "Iloveyou", "Invalid password!"
@@ -52,7 +52,7 @@ if __name__ == '__main__':
     print("3. Yaml deserialization vulnerability: use string=file.yaml")
     print("4. Use of assert statements vulnerability: run program with -O argument")
     CHOICE  = input("Select vulnerability: ")
-    if CHOICE == "1": 
+    if CHOICE == "1":
         new_person = Person("Vickie")  
         print_nametag(input("Please format your nametag: "), new_person)
     elif CHOICE == "2":
@@ -64,4 +64,3 @@ if __name__ == '__main__':
     elif CHOICE == "4":
         password = input("Enter master password: ")
         authenticate(password)
-
